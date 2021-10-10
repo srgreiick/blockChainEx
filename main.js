a067d2cb58261d6b3402198c76387491ab9f5b1c
Anthrax <samgreilick@gmail.com>
2021-10-10 09:19:11 -0600

    validator functional

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ class Block{
         this.index = index;
         this.timeStamp = timeStamp;
         this.data = data;
-        this. previousHash = previousHash;
+        this.previousHash = previousHash;
         this.hash = this.calculateHash();
     }
 
@@ -32,10 +32,34 @@ class BlockChain{
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid(){
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
+
 
 let toxyCoin = new BlockChain();
 toxyCoin.addBlock(new Block(1, "05/03/2021", {ammount: 4}))
 toxyCoin.addBlock(new Block(2, "06/03/2021", {ammount: 50}))
 
-console.log(JSON.stringify(toxyCoin, null, 4));
+//console.log(JSON.stringify(toxyCoin, null, 4));
+console.log("Valid? "+toxyCoin.isChainValid());
+
+toxyCoin.chain[1].data = {ammount:300}
+toxyCoin.chain[1].calculateHash()
+
+console.log("Valid? "+toxyCoin.isChainValid());
